6ae702824e5b1531fbb88ce9cd27325adcf33f78
Chris Ramesh <chris.ramesh@gmail.com>
2022-12-28 19:38:23 -0600

    added drag and drop with filereader approach

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
+import React, { useState, useEffect } from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [uploadedImg, setUploadedImg] = useState(null);
+
+  // creates an object url for the image
+  const handlePaste = (e) => {
+    e.preventDefault();
+    // get the items
+    const items = e.clipboardData.items;
+    // find a first image and set it
+    for (let item of items) {
+      console.log(item.type);
+      if (item.type.startsWith("image/")) {
+        console.log("image data", item);
+        let imgBlob = item.getAsFile();
+        console.log("image data", imgBlob);
+        let imgUrl = URL.createObjectURL(imgBlob);
+        console.log("img url", imgUrl);
+        setUploadedImg(imgUrl);
+        break;
+      }
+    }
+  };
+
+  // a different way to do it using drag and drop and file reader
+  // first, we need to prevent the drag behavior from opening the file so the drop event can take over
+  const blockDragOver = (e) => e.preventDefault();
+  const blockDrop = (e) => e.preventDefault();
+  // on drop, use file reader to set uploaded image
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const items = e.dataTransfer.files;
+    for (let item of items) {
+      if (item.type.startsWith("image/")) {
+        console.log("image data", item);
+        // create a reader
+        const reader = new FileReader();
+        // tell it to set image after loading stuff
+        reader.onloadend = () => {
+          console.log("img data url", reader.result);
+          setUploadedImg(reader.result);
+        };
+        // tell reader to load item as url
+        reader.readAsDataURL(item);
+        break;
+      }
+    }
+  };
+
   const renderPlaceholder = () => {
     return (
-      <div className="relative">
+      <div className="relative" onDrop={handleDrop} onDragOver={blockDragOver}>
         <div className="absolute w-full h-full flex justify-center flex-wrap content-center">
           <p>Drag and Drop Image Here</p>
         </div>
         <svg
-          class="h-64 w-full rounded border-2 border-dashed border-gray-300 bg-white text-gray-200"
+          className="h-64 w-full rounded border-2 border-dashed border-gray-300 bg-white text-gray-200"
           preserveAspectRatio="none"
           stroke="currentColor"
           fill="none"
@@ -17,8 +66,8 @@ function App() {
           aria-hidden="true"
         >
           <path
-            vector-effect="non-scaling-stroke"
-            stroke-width="2"
+            vectorEffect="non-scaling-stroke"
+            strokeWidth="2"
             d="M0 0l200 200M0 200L200 0"
           ></path>
         </svg>
@@ -26,14 +75,25 @@ function App() {
     );
   };
 
+  const renderImg = () => {
+    return (
+      <div className="relative">
+        <img src={uploadedImg} alt="Uploaded Image" />
+      </div>
+    );
+  };
+
   return (
-    <div className="App h-screen">
+    <div className="App h-screen" onDragOver={blockDragOver} onDrop={blockDrop}>
       {/* container */}
       <div className="h-full mx-auto max-w-7xl sm:px-6 lg:px-8 flex flex-col justify-center">
         {/* card */}
-        <div className="overflow-hidden rounded-lg bg-white shadow px-4 py-5 sm:p-6 ">
+        <div
+          className="overflow-hidden rounded-lg bg-white shadow px-4 py-5 sm:p-6"
+          onPaste={handlePaste}
+        >
           {/* card content */}
-          {renderPlaceholder()}
+          {uploadedImg ? renderImg() : renderPlaceholder()}
         </div>
       </div>
     </div>
